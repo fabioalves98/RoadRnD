27b8b407de9243edc92552d71119fea6d4fe0d9b
Fábio Alves <fabioalves98@ua.pt>
2020-11-02 19:11:54 +0000

    compositor init in gin-gonic

--- app/road_rnd_app/lib/main.dart
+++ app/road_rnd_app/lib/main.dart
@@ -1,9 +1,4 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,101 +6,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'RoadRnD',
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: Randomwords(),
+      home: ListCars(),
     );
   }
 }
 
-class Randomwords extends StatefulWidget {
+class ListCars extends StatefulWidget {
   @override
-  _RandomwordsState createState() => _RandomwordsState();
+  _ListCarsState createState() => _ListCarsState();
 }
 
-class _RandomwordsState extends State<Randomwords> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
+class _ListCarsState extends State<ListCars> {
+  final _cars = <String>['Honda', 'Chevrolet', 'BMW'];
   final _biggerFont = TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
+    return ListView.separated(
+      padding: EdgeInsets.all(16.0),
+      itemCount: _cars.length,
+      itemBuilder: /*1*/ (context, index) {
+        return _buildRow(_cars[index]);
       },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+  Widget _buildRow(String pair) {
+    return ListTile(
+      title: Text(
+        pair,
+        style: _biggerFont,
       ),
+      onTap: () {
+        print('Taped');
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
+      appBar: AppBar(title: Text('RoadRnD')),
       body: _buildSuggestions(),
     );
   }
