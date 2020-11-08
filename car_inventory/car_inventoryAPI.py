06f82452df33489bc9db1ab4e787f797360ae305
Diogo Marques <d.marques@ua.pt>
2020-11-08 15:28:52 +0000

    Gets and post is not perfect but working with the DB

--- car_inventory/car_inventoryAPI.py
+++ car_inventory/car_inventoryAPI.py
@@ -18,6 +18,55 @@ class MongoAPI:
         self.collection = cursor[collection]
         self.data = data
 
+        documents = self.collection.find()
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        if len(output) == 0:
+            car1 = {
+                "id": "AB-50-DF",
+                "brand": "Opel",
+                "model": "Astra",
+                "kms": 0,
+                "registrationYear": "2010",
+                "fuelType": "gasoline",
+                "owner": "Homer Simpson",
+                "photo" : "",
+                "num_of_seats": 2
+            }
+            car2 = {
+                "id": "AB-50-AA",
+                "brand": "Audi",
+                "model": "A3",
+                "kms": 0,
+                "registrationYear": "2005",
+                "fuelType": "diesel",
+                "owner": "Homer Simpson",
+                "photo" : "",
+                "num_of_seats": 5
+            }
+            car3 = {
+                "id": "AB-50-DS",
+                "brand": "Kia",
+                "model": "Ceed",
+                "kms": 0,
+                "registrationYear": "2009",
+                "fuelType": "gasoline",
+                "owner": "Homer Simpson",
+                "photo" : "",
+                "num_of_seats": 5
+            }
+            car4 = {
+                "id": "BB-50-BB",
+                "brand": "Opel",
+                "model": "Corsa",
+                "kms": 0,
+                "registrationYear": "2000",
+                "fuelType": "gasoline",
+                "owner": "Homer Simpson",
+                "photo" : "",
+                "num_of_seats": 7
+            }
+            self.collection.insert_many([car1, car2, car3, car4])
+
     def read(self):
         log.info('Reading All Data')
         documents = self.collection.find()
@@ -26,7 +75,7 @@ class MongoAPI:
 
     def write(self, data):
         log.info('Writing Data')
-        new_document = data['Document']
+        new_document = data["Document"]
         response = self.collection.insert_one(new_document)
         output = {'Status': 'Successfully Inserted',
                   'Document_ID': str(response.inserted_id)}
@@ -47,31 +96,69 @@ class MongoAPI:
         output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
         return output
 
+    def findID(self, id):
+        documents = self.collection.find({"id" : id})
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+    def findFilters(self, fuel  , brand , model , year  , seats  ):
+        query = {}
+        if(fuel != None):
+            query["fuelType"] = fuel
+        if(brand != None):
+            query["brand"] = brand
+        if(model != None):
+            query["model"] = model
+        if(year != None):
+            query["registrationYear"] = year
+        if(seats != None):
+            query["num_of_seats"] = seats
+        #return query
+        documents = self.collection.find(query)
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+
 
 @app.route('/')
 def base():
-    return Response(response=json.dumps({"Status": "DOWN"}),
+    return Response(response=json.dumps({"Status": "UP"}),
                     status=200,
                     mimetype='application/json')
 
 @app.route('/brands', methods=['GET'])
 def get_brands():
-    response = ['Audi', 'BMW', 'Opel' ]
-    return Response(response=json.dumps(response),
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
+    }
+    obj1 = MongoAPI(db)
+    response = obj1.read()
+    brands = []
+    for cars in response:
+        if(cars["brand"] not in brands):
+            brands.append(cars["brand"])
+    return Response(response=json.dumps(brands),
                     status=200,
                     mimetype='application/json')
 
 @app.route('/<brand>/models', methods=['GET'])
 def get_models(brand):
-    if(brand == 'opel'):
-        response = ['Corsa', 'Astra']
-        return Response(response=json.dumps(response),
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
+    }
+    obj1 = MongoAPI(db)
+    response = obj1.findFilters( None  , brand , None , None  , None)
+    if(len(response) > 0):
+        models = []
+        for cars in response:
+            if(cars["model"] not in models):
+                models.append(cars["model"])
+        return Response(response=json.dumps(models),
                         status=200,
-                        mimetype='application/json') 
-    response = ['A4', 'A3', 'A7' ]
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
+                        mimetype='application/json')
+    return Response(response=json.dumps("Not found"),
+                        status=404,
+                        mimetype='application/json')
 
 @app.route('/car/findByFilters', methods=['GET'])
 def get_cars_by_filters():
@@ -79,6 +166,7 @@ def get_cars_by_filters():
     brand = ''
     model = ''
     year = ''
+    seats = ''
     try:
         fuel = request.args.get('fuel')
     except:
@@ -95,30 +183,25 @@ def get_cars_by_filters():
         year = request.args.get('year')
     except:
         pass
+    try:
+        seats = request.args.get('seats')
+    except:
+        pass
 
-    car1 = {
-    "id": "AB-56-DS",
-    "brand": "Kia",
-    "model": "Ceed",
-    "kms": 0,
-    "registrationYear": "string",
-    "fuelType": "gasoline",
-    "owner": "Homer Simpson",
-    "photo" : None
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
     }
-    car2 = {
-    "id": "AB-50-DS",
-    "brand": "Opel",
-    "model": "Astra",
-    "kms": 0,
-    "registrationYear": "string",
-    "fuelType": "gasoline",
-    "owner": "Homer Simpson",
-    "photo" : None
-    }
-    return Response(response=json.dumps([car1, car2]),
-                    status=200,
-                    mimetype='application/json')
+    obj1 = MongoAPI(db)
+    response = obj1.findFilters( fuel  , brand , model , year  , seats)
+    if(len(response) > 0):
+        return Response(response=json.dumps(response),
+                        status=200,
+                        mimetype='application/json')
+    return Response(response=json.dumps("Not found"),
+                        status=404,
+                        mimetype='application/json')
+
 
 
 @app.route('/car/<carId>', methods=['GET'])
@@ -131,48 +214,68 @@ def get_car_byID(carId):
     "registrationYear": "string",
     "fuelType": "gasoline",
     "owner": "Homer Simpson",
-    "photo" : None
+    "photo" : None,
+    "num_of_seats": 5
     }
-    return Response(response=json.dumps(car),
-                    status=200,
-                    mimetype='application/json')
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
+    }
+    obj1 = MongoAPI(db)
+    response = obj1.findID(carId)
+    if(len(response) > 0):
+        return Response(response=json.dumps(response),
+                        status=200,
+                        mimetype='application/json')
+    return Response(response=json.dumps("Not found"),
+                        status=404,
+                        mimetype='application/json')
+
+
 
 
 @app.route('/car', methods=['GET'])
 def get_all_cars():
-
-    car1 = {
-    "id": "AB-56-DS",
-    "brand": "Kia",
-    "model": "Ceed",
-    "kms": 0,
-    "registrationYear": "string",
-    "fuelType": "gasoline",
-    "owner": "Homer Simpson",
-    "photo" : None
+    
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
     }
-    car2 = {
-    "id": "AB-50-DS",
-    "brand": "Opel",
-    "model": "Astra",
-    "kms": 0,
-    "registrationYear": "string",
-    "fuelType": "gasoline",
-    "owner": "Homer Simpson",
-    "photo" : None
-    }
-    return Response(response=json.dumps([car1, car2]),
+    obj1 = MongoAPI(db)
+    response = obj1.read()
+    return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
+
+
+@app.route('/car', methods=['POST'])
+def post_car():
+    data = request.json
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
+    }
+    obj1 = MongoAPI(db)
+    unique = obj1.findID(data["id"])
+    if(len(unique) > 0):
+        return Response(response=json.dumps("Car already in DB"),
+                    status=403,
+                    mimetype='application/json')
+    doc = {"Document" : data}
+    response = obj1.write(doc)
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
 
 @app.route('/mongodb', methods=['GET'])
 def mongo_read():
     data = request.json
-    if data is None or data == {}:
-        return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
-    obj1 = MongoAPI(data)
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
+    }
+    obj1 = MongoAPI(db)
     response = obj1.read()
     return Response(response=json.dumps(response),
                     status=200,
@@ -182,16 +285,15 @@ def mongo_read():
 @app.route('/mongodb', methods=['POST'])
 def mongo_write():
     data = request.json
-    if data is None or data == {} or 'Document' not in data:
-        return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
-    obj1 = MongoAPI(data)
+    db = {
+        "database" : "carInventory",
+        "collection" : "cars"
+    }
+    obj1 = MongoAPI(db)
     response = obj1.write(data)
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
-
 @app.route('/mongodb', methods=['PUT'])
 def mongo_update():
     data = request.json
@@ -222,3 +324,6 @@ def mongo_delete():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
+    
+
+    
