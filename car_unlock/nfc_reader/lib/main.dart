b25518357e56d85ddea0fe9c2d3ee9af518f0d51
Marco Silva <masilva1998@hotmail.com>
2020-12-17 17:31:59 +0000

    NFC read library made more generic

--- car_unlock/nfc_reader/lib/main.dart
+++ car_unlock/nfc_reader/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 import 'nfc_read.dart';
 
 void main() => runApp(MaterialApp(home: MainApp()));
@@ -10,7 +9,7 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
-  NFCRead nfc = NFCRead();
+  NFCRead nfc = NFCRead("http://40.115.31.209:5003");
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +29,7 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
                     if (snapshot.data != "NFC Not Available" &&
                         snapshot.data != "Reading NFC Tag") {
                       res = snapshot.data;
-                      return Center(
+                      /* return Center(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
@@ -40,7 +39,37 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
                             ),
                           ],
                         ),
-                      );
+                      ); */
+                      return FutureBuilder(
+                          /* Dummy unlock test */
+                          future: nfc.unlock("AA-01-AA", "tag1"),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> snapshot) {
+                            if (snapshot.hasData) {
+                              res = snapshot.data;
+                              return Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    /* Should appear response body from service */
+                                    Text(
+                                      'Platform: ${nfc.getPlataform()} \nTag ID: $res',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              );
+                            } else {
+                              return Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text('Cenas'),
+                                  ],
+                                ),
+                              );
+                            }
+                          });
                     } else {
                       return Center(
                         child: Column(
--- car_unlock/nfc_reader/lib/nfc_read.dart
+++ car_unlock/nfc_reader/lib/nfc_read.dart
@@ -6,6 +6,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 
 class NFCRead {
+  final String host;
+  NFCRead(this.host);
+
   String getPlataform() {
     return '${Platform.operatingSystem} ${Platform.operatingSystemVersion}';
   }
@@ -60,299 +63,83 @@ class NFCRead {
     }
   }
 
-  Future<String> unlockCar(String license, String tag) async {
-    print("Unlock car");
-    final response = await http.post('http://40.115.31.209:5003/unlock',
+  Future<String> unlock(String id, String tag) async {
+    print("Unlock");
+    final response = await http.post(host + '/unlock',
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(<String, String>{'id': license, 'tag': tag}));
+        body: jsonEncode(<String, String>{'id': id, 'tag': tag}));
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return "Car Unlocked";
+      return response.body;
     } else {
       print(response.statusCode);
       return "Error";
     }
   }
 
-  Future<String> lockCar(String license, String tag) async {
-    print("Lock Car");
-    final response = await http.post('http://40.115.31.209:5003/lock',
+  Future<String> lock(String id, String tag) async {
+    print("Lock");
+    final response = await http.post(host + '/lock',
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(<String, String>{'id': license, 'tag': tag}));
+        body: jsonEncode(<String, String>{'id': id, 'tag': tag}));
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return "Car Locked";
+      return response.body;
     } else {
       print(response.statusCode);
       return "Error";
     }
   }
 
-  Future<String> addCar(String license, String tag) async {
-    print("Add car");
-    final response = await http.post('http://40.115.31.209:5003/add',
+  Future<String> add(String id, String tag) async {
+    print("Add");
+    final response = await http.post(host + '/add',
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(<String, String>{'id': license, 'tag': tag}));
+        body: jsonEncode(<String, String>{'id': id, 'tag': tag}));
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return "Car Added";
+      return response.body;
     } else {
       print(response.statusCode);
       return "Error";
     }
   }
 
-  Future<String> delCar(String license) async {
-    print("Delete Car");
-    final response = await http.post('http://40.115.31.209:5003/delete',
+  Future<String> delete(String id) async {
+    print("Delete");
+    final response = await http.post(host + '/delete',
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(<String, String>{'id': license}));
+        body: jsonEncode(<String, String>{'id': id}));
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return "Car Deleted";
+      return response.body;
     } else {
       print(response.statusCode);
       return "Error";
     }
   }
 
-  Future<String> updateTag(String license, String tag) async {
+  Future<String> updateTag(String id, String tag) async {
     print("Update tag");
-    final response = await http.post('http://40.115.31.209:5003/updateTag',
+    final response = await http.post(host + '/updateTag',
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(<String, String>{'id': license, 'tag': tag}));
+        body: jsonEncode(<String, String>{'id': id, 'tag': tag}));
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return "Tag updated";
+      return response.body;
     } else {
       print(response.statusCode);
       return "Error";
     }
   }
 }
-
-/* class _NFCReadState extends State<NFCRead> with SingleTickerProviderStateMixin {
-  String _platformVersion =
-      '${Platform.operatingSystem} ${Platform.operatingSystemVersion}';
-  NFCAvailability _availability = NFCAvailability.not_supported;
-  NFCTag _tag;
-  String _result;
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    NFCAvailability availability;
-    try {
-      availability = await FlutterNfcKit.nfcAvailability;
-    } on PlatformException {
-      availability = NFCAvailability.not_supported;
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      // _platformVersion = platformVersion;
-      _availability = availability;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> res = ["", ""];
-    return Scaffold(
-      appBar: AppBar(title: Text('RoadRnD')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Running on: $_platformVersion\n $_availability'),
-            FutureBuilder(
-                future: readTag(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<String>> snapshot) {
-                  if (snapshot.hasData) {
-                    res = snapshot.data;
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'ID: ${res[0]} \nTransceive Result: ${res[1]}',
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    );
-                    /* switch (widget.op) {
-                      case "unlock":
-                        {
-                          return FutureBuilder(
-                              future: unlockCar(widget.licensePlate),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasData) {
-                                  return Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          'ID: ${res[0]} \nTransceive Result: ${res[1]} \n${snapshot.data}',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }
-                              });
-                        }
-                        break;
-
-                      case "lock":
-                        {
-                          return FutureBuilder(
-                              future: lockCar(widget.licensePlate),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasData) {
-                                  return Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          'ID: ${res[0]} \nTransceive Result: ${res[1]} \n${snapshot.data}',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }
-                              });
-                        }
-                        break;
-
-                      case "add":
-                        {
-                          return FutureBuilder(
-                              future: addCar(widget.licensePlate),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasData) {
-                                  return Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          'ID: ${res[0]} \nTransceive Result: ${res[1]} \n${snapshot.data}',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }
-                              });
-                        }
-                        break;
-
-                      case "delete":
-                        {
-                          return FutureBuilder(
-                              future: delCar(widget.licensePlate),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasData) {
-                                  return Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          'ID: ${res[0]} \nTransceive Result: ${res[1]} \n${snapshot.data}',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }
-                              });
-                        }
-                        break;
-
-                      case "update":
-                        {
-                          return FutureBuilder(
-                              future: updateTag(widget.licensePlate),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasData) {
-                                  return Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          'ID: ${res[0]} \nTransceive Result: ${res[1]} \n${snapshot.data}',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }
-                              });
-                        }
-                        break;
-                      default:
-                        {
-                          return Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text('Reading NFC Tag'),
-                              ],
-                            ),
-                          );
-                        }
-                        break;
-                    } */
-                  } else {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text('Reading NFC Tag'),
-                        ],
-                      ),
-                    );
-                  }
-                }),
-          ],
-        ),
-      ),
-    );
-  }
-} */
